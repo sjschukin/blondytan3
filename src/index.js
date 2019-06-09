80252b92a304fe201ac915f143ce9446716356ab
Sergey Shchukin <s.j.schukin@yandex.ru>
2019-06-09 22:17:10 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './js/_bootstrap.js';
+import './js/_scripts.js';
+
+import './scss/_styles.scss'
---
+++ src/js/_bootstrap.js
@@ -0,0 +1,3 @@
+import '../../node_modules/bootstrap/js/dist/util.js';
+import '../../node_modules/bootstrap/js/dist/scrollspy.js';
+import '../../node_modules/bootstrap/js/dist/collapse.js';
